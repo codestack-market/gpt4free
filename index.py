7549a8dfc030016662ce2783e3a0f6274e771452
RedMythic1 <ab7552@pleasantonusd.net>
2023-10-24 20:28:20 -0700

    q

---
+++ index.py
@@ -0,0 +1,67 @@
+import os
+import discord
+from discord.ext import commands
+from discord.commands import Option
+from flask import render_template, Flask
+app = Flask(__name__, template_folder='templates')
+
+
+import g4f
+from g4f.Provider import (
+    AItianhu,
+    Aichat,
+    Bard,
+    Bing,
+    ChatBase,
+    ChatgptAi,
+    OpenaiChat,
+    Vercel,
+    You,
+    Yqcloud,
+)
+
+
+# Load your API key from an environment variable or secret management service
+
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(intents=intents)
+
+@app.route('/')
+def index():
+    return "Running"
+@bot.event
+async def on_ready():
+	print(f'Logged in as {bot.user.name}, {bot.user.id}, ping is {bot.latency}ms')
+	print(bot.user.name)
+	print(bot.user.id)
+
+@bot.slash_command(name='text', description='Generate text using your prompt')
+async def text(ctx, prompt: Option(str, "Enter your prompt", required = True, default = '')):
+	await ctx.respond(f'<@!{ctx.author.id}>, Generating from prompt: {prompt}', delete_after = 2)
+	def generate_prompt():
+		return f"""{prompt}"""
+	if 1==1:
+		response = g4f.ChatCompletion.create(
+    model=g4f.models.gpt_4,
+    provider=Bing,
+    messages=[{"role": "user", "content": generate_prompt()}],
+)  # altern
+		final = response.strip('Using Bing Provider')
+		if "code" in prompt:
+			message =  await ctx.send('Generating...')
+			print('No errors')
+			await message.edit(f"""<@!{ctx.author.id}>```
+	    {final}
+	```""")
+			print(bot.latency)
+		elif "code" not in prompt:
+			message = await ctx.send('Generating...')
+			print('No errors')
+			await message.edit(f"<@!{ctx.author.id}>{final}")
+			print(bot.latency)  
+
+if __name__ == "__main__":
+        app.run(host='0.0.0.0', debug=True)
+
+bot.run("MTE2NjUzOTIwMDA2NjIzNjQ2Ng.GaoYag.bPgppBFWV1LpToT_TEd62IR-3Da2MwbcvzWucU")
