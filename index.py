bae02ba22ffda6907840e2b613e3275a64b8ecaf
Avneh Singh Bhatia <98300720+RedMythic1@users.noreply.github.com>
2023-10-24 20:42:04 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 import os
 import discord
 from discord.ext import commands
-from discord.ext.commands import Option
 from flask import render_template, Flask
 app = Flask(__name__, template_folder='templates')
 
